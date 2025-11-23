202d4e714184fe9cf1105735537c319370b2429d
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-23 00:16:59 -0500

    rename database in app.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ExpenseScreen from './ExpenseScreen';
 
 export default function App() {
   return (
-    <SQLiteProvider databaseName="expenses.db">
+    <SQLiteProvider databaseName="expenses_v2.db">
       <ExpenseScreen />
     </SQLiteProvider>
   );
